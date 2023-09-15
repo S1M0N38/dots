979697ab38510212f76d7df7bfe6c171086db0e0
S1M0N38 <bertolottosimone@gmail.com>
2023-09-15 12:23:34 +0200

    LSP configs
    - disable "Unused label" warning in texlab
    - ltex configuration
    - disable ruff lsp hover in favor of pyright

--- config/nvim/lua/plugins/lspconfig.lua
+++ config/nvim/lua/plugins/lspconfig.lua
@@ -17,9 +17,33 @@ return {
               executable = "/Applications/Skim.app/Contents/SharedSupport/displayline",
               args = { "-g", "%l", "%p", "%f" },
             },
+            diagnostics = {
+              -- Ignore "Unused label" warning. I like to put labels on sections, subsections, etc.
+              ignoredPatterns = { "Unused label" },
+            },
           },
         },
       },
+      ltex = {
+        settings = {
+          ltex = {
+            enabled = { "tex", "bib" },
+            language = "en",
+          },
+        },
+      },
+      pyright = {},
+      ruff_lsp = {},
+    },
+    setup = {
+      ruff_lsp = function()
+        require("lazyvim.util").on_attach(function(client, _)
+          if client.name == "ruff_lsp" then
+            -- Disable hover in favor of Pyright
+            client.server_capabilities.hoverProvider = false
+          end
+        end)
+      end,
     },
   },
 }
