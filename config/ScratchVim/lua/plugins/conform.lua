9c77cf0cbcfe732b70a96d665bb4d4876c2f123e
S1M0N38 <bertolottosimone@gmail.com>
2023-12-29 11:58:47 +0100

    Add latexintent to formatter

--- config/ScratchVim/lua/plugins/conform.lua
+++ config/ScratchVim/lua/plugins/conform.lua
@@ -15,12 +15,17 @@ return {
   opts = {
     formatters_by_ft = {
       lua = { "stylua" },
-      python = { "ruff_format", "ruff_fix " },
+      python = { "ruff_format", "ruff_fix" },
+      tex = { "latexindent" },
     },
     -- Set up format-on-save
     format_on_save = { timeout_ms = 500, lsp_fallback = true },
     -- Customize formatters
-    formatters = {},
+    formatters = {
+      latexindent = {
+        prepend_args = { "-l" }
+      }
+    },
   },
   init = function()
     -- If you want the formatexpr, here is the place to set it
